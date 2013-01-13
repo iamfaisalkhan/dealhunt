90426935c4cc73199a350dac1d1a78988b1e9395
Faisal Khan <faisal@Faisals-MacBook-Air.local>
2013-01-13 15:03:14 -0600

    Added a new column in sub_category table.

--- bbY/db/bbY.sql
+++ bbY/db/bbY.sql
@@ -2,15 +2,16 @@
 use bbY;
 
 
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS category (
    id INT NOT NULL AUTO_INCREMENT,
    eight_coupon_id INT,
    title varchar(150),
    PRIMARY KEY(id)
 );
 
-CREATE TABLE sub_category (
+CREATE TABLE IF NOT EXISTS sub_category (
    id INT NOT NULL AUTO_INCREMENT,
+   category_id INT NOT NULL,
    eight_coupon_id INT,
    title varchar(150),
    PRIMARY KEY(id)
