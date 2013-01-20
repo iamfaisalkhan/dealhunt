45f4c61cd17e697a8ed5441dc27deca9c8896669
Faisal Khan <faisal.nust@gmail.com>
2013-01-20 15:29:26 -0600

    Updated template header and footer files using bootstrap template.

--- bbY/application/views/templates/footer.php
+++ bbY/application/views/templates/footer.php
@@ -1,3 +1,21 @@
-<strong>&copy; 2011</strong>
-</body>
-</html>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url();?>/assets/js/jquery.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-transition.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-alert.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-modal.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-tab.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-popover.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-button.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap-typeahead.js"></script>
+    
+  </head>
+
+  <body>
