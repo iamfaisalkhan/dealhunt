35b8db2c611245f22fd60a8eab9120ad0f533e6c
phyizal <faisal.nust@gmail.com>
2013-01-22 09:49:46 -0600

    Display the deal text along with the image. playing with the layout.

--- bbY/application/views/user/user_view.php
+++ bbY/application/views/user/user_view.php
@@ -1,61 +1,84 @@
 <div class="container-fluid">
  <div class="row-fluid">
- 
+
   <div class="span2">
    <!--  Sidebar -->
   </div>
- 
+
   <div class="span10">
    <!--  Body  -->
-  
-   <div class="row-fluid">
-      <div class="span6">
-       <table class="table">
+
+   <div class="row">
+    <div class="span8">
+     <table class="table">
+      <tr>
+       <?php $count = 0;?>
        <?php foreach ($deals as $deal):?>
-        <tr>
-         <td><a href="<?php echo $deal->url;?>"><img style="width:150px; height:auto;" src="<?php echo $deal->image?>" class="img-polaroid"></a></td>
-        </tr>
-        <?php endforeach;?>
-        </table>
-       </div>
-      </div>
-      <div class="span2">
-       <form class="form-inline" action="<?php echo base_url();?>/index.php/user/add"  method="post">
-        <table class="table">
-         <tr>
-          <td style="border-top:none;"><input name="item" type="text" placeholder="Tell us what you love!" /> </td>
-          <td  style="border-top:none;"><button type="submit" class="btn">Add</button> </td>
-         </tr>
-        </form>
-   
-        <!--  display all user products  -->
-        <?php foreach ($products as $product):?>
-        <tr>
-         <td><?php echo $product->name?> </td>
-         <td style="text-align: right;">
-            <!--<a href="<?php echo base_url()?>/user/edit/<?php echo $product->id?>"><i class="icon-pencil"></i></a> 
-             &nbsp;-->
-             <a href="<?php echo base_url()?>/user/del/<?php echo $product->id?>"><i class="icon-trash"></i> </a>
-         </td>
-        </tr>
-        <?php endforeach;?>
-        
-        <!--  if the prodcts list was empty -->
-        <?php if (count($products) == 0) : ?>
-        <tr>
-         <td colspan="2" style="text-align: center;">
-          <small>Add items to get great offers</small>
-         </td>
-        </tr>
-        <?php endif;?>
-        <!--  for display an extra line at the end of the table. -->
-        <tr>
-         <td colspan="2"></td>
-        </tr>
-       </table>
-   
-      </div>
-  </div>  <!-- span10 side bar -->
+       <td style="border: none;">
+         <div class="span10 wrap">
+          <a href="<?php echo $deal->url;?>">
+           <img src="<?php echo $deal->image?>" class="img-polaroid">
+          </a>
+          <div class="span10">
+          <?php echo trim($deal->txt);?>
+          </div>
+         </div>
+       </td>
+       <?php $count = $count + 1;?>
+       <?php if ( ($count % 4) == 0):?>
+      </tr>
+      <!-- Start a new row, if there are still more elements -->
+      <?php if (count($deals) > $count):?>
+      <tr>
+      <?php endif;?>
+      
+      <?php endif;?>
+
+       <?php endforeach;?>
+     
+     </table>
+    </div>
+    <div class="span2">
+     <form class="form-inline"
+      action="<?php echo base_url();?>/index.php/user/add" method="post">
+      <table class="table">
+       <tr>
+        <td style="border-top: none;"><input name="item" type="text"
+         placeholder="Tell us what you love!" /></td>
+        <td style="border-top: none;"><button type="submit" class="btn">Add</button>
+        </td>
+       </tr>
+       </form>
+
+       <!--  display all user products  -->
+       <?php foreach ($products as $product):?>
+       <tr>
+        <td><?php echo "$product->name"?></td>
+        <td style="text-align: right;">
+         <a href="<?php echo base_url()?>/user/del/<?php echo $product->id?>"><i
+          class="icon-trash"></i> </a>
+        </td>
+       </tr>
+       <?php endforeach;?>
+
+       <!--  if the prodcts list was empty -->
+       <?php if (count($products) == 0) : ?>
+       <tr>
+        <td colspan="2" style="text-align: center;"><small>Add items to
+          get great offers</small></td>
+       </tr>
+       <?php endif;?>
+       <!--  for display an extra line at the end of the table. -->
+       <tr>
+        <td colspan="2"></td>
+       </tr>
+      </table>
+    
+    </div>
+   </div>
+  </div>
+  <!-- span10 side bar -->
 
  </div>
-</div> <!-- /container -->
+</div>
+<!-- /container -->
