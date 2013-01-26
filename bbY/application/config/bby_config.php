920360f34816b08d710473fc715ef7ff2400fd88
phyizal <faisal.nust@gmail.com>
2013-01-26 00:03:18 -0600

    Added Facebook api information to config array.

--- bbY/application/config/bby_config.php
+++ bbY/application/config/bby_config.php
@@ -3,6 +3,8 @@
 
 // The api key for 8coupons API
 $config['8coupons_api_key']='1de19c6e2f7bf4ec46cb84b76b455d64c07c197a2462cf4640645f68d2b1b6a7b02801cc53a728a5f4e7e8616ad3cace';
+$config['fb_secret'] = 'c5aa362252715c02a7e48a128e85bd9e';
+$config['fb_appID'] = '377429082349634';
 
 
 ?>
