8d17421c17a02d252361deac9be25df6a3a7d6c6
Faisal Khan <faisal.nust@gmail.com>
2013-01-29 22:35:39 -0600

    Added javascript files for boot-strap. Added project specific js file.

---
+++ bbY/assets/js/budget-buy.js
@@ -0,0 +1,59 @@
+$(function() {  
+
+    $('.add_item').click(function() {
+        var item_id = $(this).attr('id');
+        enableEditBox($(this).parent().parent(), item_id);
+    });
+}); 
+
+function enableEditBox(item, item_id) {
+     item.replaceWith(
+           '<tr><td><p><input id="' + item_id + '" type="text" placeholder="Shopping Item" class="negative-margin"></p></td></tr>'
+     );
+     $('#' + item_id).focus();
+     $('#' + item_id).on("keydown", function(e) {
+        if (e.which == 13) 
+        {
+            var el = $(this).val();
+            if (! el) return;
+            submitItem(el, item_id);
+
+            return false;
+        }
+
+    });
+}
+
+function submitItem(value, item_id) {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost/bbY/user/ajax_test",
+        data: "elect_item=" + value,
+        success: function(html) {
+            var response = JSON.parse(html);
+            if (response.status == 1) {
+                // Get the parent TD element. 
+                var td_el = $('#' + item_id).parent().parent().parent();
+                td_el.replaceWith(
+                    '<tr><td>' + value + '</td></tr>' + 
+                    '<tr><td> <a href="#" id="' + item_id + '" class="add_item"><i class="icon-plus-sign"> </i><em> add.. </em> </a></td></tr>'
+                );
+
+                $('.add_item').off('click');
+
+                $('.add_item').on('click', function() {
+                    var item_id = $(this).attr('id');
+                    enableEditBox($(this).parent().parent(), item_id);
+                });
+            	
+                $('#message').css('visibility', 'hidden');
+        	} else {
+        		$('#message').css('visibility', 'visible');
+        	}
+
+        },
+        error: function() {
+        	$('#message').css('visibility', 'visible');
+        }
+    });
+}
